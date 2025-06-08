e944617fd2cf50aa3ccc3caddef118757d0810e5
Robert le Clus <rleclus@gmail.com>
2025-06-08 21:32:32 +0200

    Updated goal week to have a week start date for matching goals to actual workouts

--- Sources/GoalManager/Models/GoalWeek.swift
+++ Sources/GoalManager/Models/GoalWeek.swift
@@ -11,10 +11,12 @@ import SwiftData
 public class GoalWeek: Identifiable {
 	@Attribute(.unique) public var id: UUID
 	public var goalName: String
+	public var startDate: Date
 	public var goalsForWeek: [Goal]
-	public init(id: UUID, goalName: String, goalsForWeek: [Goal]) {
+	public init(id: UUID, goalName: String, startDate: Date, goalsForWeek: [Goal]) {
 		self.id = id
 		self.goalName = goalName
+		self.startDate = startDate
 		self.goalsForWeek = goalsForWeek
 	}
 }
--- Sources/GoalManager/Services/GoalDataService.swift
+++ Sources/GoalManager/Services/GoalDataService.swift
@@ -13,14 +13,35 @@ public class GoalDataService {
 		self.context = context
 	}
 	
-	public func addGoals(name: String, goals: [Goal]) throws {
-		let week = GoalWeek(id: UUID(), goalName: name, goalsForWeek: goals)
+	public func addGoals(name: String, startDate:Date, goals: [Goal]) throws {
+		let week = GoalWeek(id: UUID(), goalName: name, startDate: startDate, goalsForWeek: goals)
 		context.insert(week)
 		try context.save()
 	}
 	
 	public func fetchGoals() throws -> [GoalWeek] {
 		try context.fetch(FetchDescriptor<GoalWeek>())
+	}
+	
+	public func fetchGoals(for weekStarting: Date) throws -> GoalWeek? {
+		let calendar = Calendar.current
+		let startOfDay = calendar.startOfDay(for: weekStarting)
+		guard let nextDay = calendar.date(byAdding: .day, value: 1, to: startOfDay) else {
+			return nil
+		}
+		
+		let descriptor = FetchDescriptor<GoalWeek>(
+			predicate: #Predicate { goal in
+				goal.startDate >= startOfDay && goal.startDate < nextDay
+			}
+		)
+		
+		let weeks = try context.fetch(descriptor)
+		if weeks.count > 0 {
+			return weeks[0]
+		} else {
+			return nil
+		}
 	}
 	
 	public func makeGoals(from goalsData: [(Int,Int,Int,String)]) -> [Goal] {
@@ -30,5 +51,4 @@ public class GoalDataService {
 		}
 		return goals
 	}
-
 }
